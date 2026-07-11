ad3826d89ff4ccb9286c496b4bdb566bf2060e84
marycamara <marycamara101@gmail.com>
2026-07-11 02:10:07 +0100

    feat: add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import Navbar from './components/Navbar/Navbar'
-import './App.css'
-
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Navbar />
-    </div>
-  )
 
+      <main className="main-content"></main>
+
+      <Footer />
+    </div>
+  );
 }
-      
-export default App
+
+export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div> Built and designed by Mary Camara</div>
+      <div>All right reserved © </div>
+    </div>
+  );
+}
