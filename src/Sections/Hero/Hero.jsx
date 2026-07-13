5b59dc062bdf90934344ce26574ce4eb8774597a
marycamara <marycamara101@gmail.com>
2026-07-13 21:27:33 +0100

    feat: add hero section

---
+++ src/Sections/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import './Hero.css';
+
+export default function Hero() {
+  return (
+    <section className="hero" id="home">
+      <h1>
+        hi, <span className="hightlight">mary</span> here
+      </h1>
+      <p>Software Engineer in London</p>
+    </section>
+  );
+}
