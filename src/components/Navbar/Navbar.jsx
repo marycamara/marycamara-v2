87d3cebfdf517ad9f7d066655eccbbcf81755a27
marycamara <marycamara101@gmail.com>
2026-07-19 22:15:07 +0100

    feat(navbar): add social media icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,7 @@
+import GitHubIcon from '@mui/icons-material/Github';
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
 import './Navbar.css';
 
 export default function Navbar() {
@@ -21,6 +25,26 @@ export default function Navbar() {
           <a href="contact"></a>
         </li>
       </ul>
+
+      <div className="nav-icons">
+        <a href="marycamara@gmail.com" target="_blank" rel="">
+          <EmailRoundedIcon />
+        </a>
+        <a href="https://github.com/marycamara" target="_blank" rel="noopener noreferrer">
+          <GitHubIcon />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/mary-c-9137b820a/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedInIcon />
+        </a>
+
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <BorderColorIcon />
+        </a>
+      </div>
     </nav>
   );
 }
